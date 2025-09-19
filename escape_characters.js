866239e1b1790bfb07bb2de5445a50ab569c17db
Nandinidasari63 <nandinidasari728@gmail.com>
2025-09-19 17:58:23 +0530

    practicing how escape characters work

---
+++ escape_characters.js
@@ -0,0 +1,14 @@
+const x = 2;
+const y = 3;
+const z = 4;
+console.log(x,"\n",y,"\n",z);
+console.log(x + "" + y + z);
+console.log(x + y + "" + z);
+console.log("\\");
+console.log("\\\"");
+let string = "\n";
+console.log(string.length)
+console.log("\'");
+
+
+
