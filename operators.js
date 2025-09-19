57ce46e444cc88d467cc46f0e73cadabb7375091
Nandinidasari63 <nandinidasari728@gmail.com>
2025-09-19 17:26:23 +0530

    opearations on airthmetic and logical operators

---
+++ operators.js
@@ -0,0 +1,18 @@
+
+let num1 = 30;
+let num2 = 40;
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num2 - num1);
+console.log(num1 * num1);
+console.log(num1 / num2);
+console.log(num1 % num2);
+console.log(num1 += num1);
+console.log(num1 += num2);
+console.log(num1 == num2);
+console.log(num2 && num1);
+console.log(num2 || num1);
+console.log(!!num2);
+console.log(++num2);
+console.log(num1 && num2 || num1);
+console.log(num1 || num2 && num1);
