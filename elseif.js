e0120effac9f0cc1ba7db4be234b043bf7d2d715
Nandinidasari63 <nandinidasari728@gmail.com>
2025-09-19 16:43:52 +0530

    checking flow of elseif

---
+++ elseif.js
@@ -0,0 +1,23 @@
+    let weekNumber = 4;
+    if(weekNumber === 1){
+
+    console.log("sunday");
+
+    } else if(weekNumber === 2){
+
+        console.log("sunday");
+
+    } else if(weekNumber === 3){
+
+        console.log("sunday");
+
+    } else if(weekNumber === 4){
+        
+        console.log("sunday");
+
+    } else{
+
+        console.log('enter corrrect number');
+    
+    }      
+    
