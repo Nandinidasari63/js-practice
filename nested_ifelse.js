076b40f1d739039c68d95b4b53cfbc11e06f7068
Nandinidasari63 <nandinidasari728@gmail.com>
2025-09-19 17:21:53 +0530

    finding biggest of 3 numbers using nested  ifelse

---
+++ nested_ifelse.js
@@ -0,0 +1,22 @@
+
+let a = 90,b = 78,c = 100;
+if(a > b){
+
+    if(a>c){
+
+        console.log("a is big");    
+
+    } else{
+
+        if(b>c){
+
+        console.log("b is big");
+
+        } else{
+
+            console.log("c is big");
+            
+        }
+        
+    }
+}
