61be2be5ab5d103c255cc0c5aa91ae29abbc46e7
Nandinidasari63 <nandinidasari728@gmail.com>
2025-09-19 17:16:53 +0530

    checking how if else works

---
+++ ifelse.js
@@ -0,0 +1,22 @@
+let age = -7;
+const cand = "Nandini";
+if(age>18 && age>0){
+
+    console.log("you can vote for ",cand);
+
+} else{
+
+    console.log("you can not vote but after the ",18-age +"years you can vote ",cand);
+    
+}
+let marks = 989;
+let prizeMoney = marks >= 989 ? 5000 : 0;
+if (prizeMoney > 0){
+
+    console.log("hurray,you got the prize money of",prizeMoney);
+
+} else{
+
+    console.log("sorry try next time");
+    
+}
