5bc896a1a909afa48ea7f35c37104542cceaa6c3
Nandinidasari63 <nandinidasari728@gmail.com>
2025-09-23 09:43:28 +0530

    finding simple interst using formula

---
+++ js assignment1/simple_interst.js
@@ -0,0 +1,12 @@
+
+// Do not rename p, t or r, use them as input for your program.
+// While testing we will change their values.
+
+const p = 100000;
+const t = 0.5;
+const r = 2;
+// Print the simple interest.
+// Printing more than one output or printing anything other than simple interest might will be consider as error.
+// START YOUR CODE AFTER THIS LINE. DO NOT REMOVE THIS LINE
+const interst = (p * t * r) / 100;
+console.log(interst);
