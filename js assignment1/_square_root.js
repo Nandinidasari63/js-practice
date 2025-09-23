f97aa1fe33a1f083a537b698fdaf1dee47c2d4c9
Nandinidasari63 <nandinidasari728@gmail.com>
2025-09-23 10:16:39 +0530

    finding square root for given number

---
+++ js assignment1/_square_root.js
@@ -0,0 +1,10 @@
+// Do not rename a, use it as input for your program.
+// While testing we will change its values.
+
+const a = 625;
+
+// Print the square root of a
+// Printing more than one output or printing anything other than square root might will be consider as error.
+// START YOUR CODE AFTER THIS LINE. DO NOT REMOVE THIS LINE
+const squareRoot = a ** 0.5;
+console.log(squareRoot);
