ad581698ab7ddc67eba14dbd496d8db1439ccb10
Nandinidasari63 <nandinidasari728@gmail.com>
2025-10-04 22:57:57 +0530

    Create next_date_tests.js

---
+++ testCases/next_date_tests.js
@@ -0,0 +1,27 @@
+| Description | Input | Output |
+| --- | --- | --- |
+| Simple increment of a normal day | 15-03-2021 | 16-03-2021 |
+|increment of month based on number of days in january | 31-01-2025 | 01-02-2025|
+| checking whether the january contains 31 or 30 days  | 30-01-2025 | 31-01-2025|
+|feb month last day in leap year | 28-02-2020 | 29-02-2020 |
+|feb month last day in not leap year | 28-02-2021 | 01-03-2021 |
+| increment of month based on number of days in march| 31-03-2025 | 01-04-2025|
+| increment of month based on number of days in april | 30-04-2025 | 01-05-2025|
+| increment of month based on number of days in may | 31-05-2025 | 01-06-2025|
+| increment of month based on number of days in june | 30-06-2025 | 01-07-2025|
+| increment of month based on number of days in july | 31-07-2025 | 01-08-2025|
+| increment of month based on number of days in august | 31-08-2025 | 01-09-2025|
+| increment of month based on number of days in september  | 30-09-2025 | 01-10-2025|
+| increment of month based on number of days in october | 31-10-2025 | 01-11-2025|
+| increment of month based on number of days in november | 30-11-2025 | 01-12-2025|
+| increment of month and year based on number of days in december | 31-12-2025 | 01-01-2026|
+| century year divided by 4 (leap year) | 28-02-2400 | 29-02-2400 |
+| century year not divided by 4 (not leap year) | 28-02-2300 | 01-03-2300|
+| centuary year not divided by 4 but it still leap year | 28-02-1900 | 29-02-1900|
+| april consists only 30 days in month | 31-04-2025 | 'invalid date'|
+| june consists only 30 days in month  | 31-06-2025 | 'invalid date'|
+| next date of the input must be in the format of dd-mm-yyyy(here we will get year as 5 digits) | 31-12-9999 | 'invalid date' |
+| date limit upto 31 | 32-08-2020 | 'invalid date'|
+| month limit to 12 | 08-19-2020 | 'invalid date'|
+| date and month are restricted to 30 or 31 and 12 | 32-13-2020 | 'invalid date'|
+| month,year,date can not be zero | 00-00-0000 | 'invalid date'|
