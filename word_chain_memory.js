747fa35e4c090ec80e9bf406f8af1655d1320aa9
Nandinidasari63 <nandinidasari728@gmail.com>
2025-10-15 16:04:43 +0530

    Update word_chain_memory.js

--- word_chain_memory.js
+++ word_chain_memory.js
@@ -16,32 +16,87 @@ function isEven(number) {
   return number % 2 === 0
 }
 
+function checkIsEqual(newWord, word) {
+  let sliceEnd = word.length;
+  //console.log(sliceEnd);
+  //console.log('newWord is', newWord);
+  //console.log('checking is it equal 1.word ===', word, '2.new word ===', newWord);
+  return word === newWord.slice(0, sliceEnd);
+}
+
+
 function play() {
   gameRules();
   let word = '';
   const wordsArray = [];
   let lastWordOfUser1;
   let lastWordOfUser2;
+  let sequenceofWords = '';
+  let pointsOfUser1 = 0;
+  let pointsOfUser2 = 0;
+  let winner;
   const user1 = prompt("enter name of the first palyer");
   const user2 = prompt('enter name of the second player');
-  for (let round = 0; round < 10; round++) {
+  for (let round = 0; round < 4; round++) {
     if (isEven(round)) {
       lastWordOfUser2 = word;
-      word = prompt(user1 + '  enter the word');
-       if (round === 0) {
+      console.log('\n');
+      console.clear();
+      word = prompt('\x1b[1m ' + user1 + '  enter the word \x1b[0m');
+      if (round === 0) {
         wordsArray.push(word);
       } else {
         const sliceStart = word.length - (word.length - lastWordOfUser2.length - 1);
-        wordsArray.push(word.slice(sliceStart, word.length));
+        if (checkIsEqual(word, lastWordOfUser2)) {
+          wordsArray.push(word.slice(sliceStart, word.length));
+        }
       }
     } else {
       lastWordOfUser1 = word;
-      word = prompt(user2 + '  enter the word');
-        const sliceStart = word.length - (word.length - lastWordOfUser1.length - 1);
-      wordsArray.push(word.slice(sliceStart, word.length))
+      console.log('\n');
+      console.clear();
+      word = prompt('\x1b[1m' + user2 + '  enter the word \x1b[0m');
+      const sliceStart = word.length - (word.length - lastWordOfUser1.length - 1);
+      if (checkIsEqual(word, lastWordOfUser1))
+        wordsArray.push(word.slice(sliceStart, word.length))
     }
-    console.log('array', wordsArray);
+    sequenceofWords = wordsArray[0];
+    // console.log('array', wordsArray,'sequenceofWords',sequenceofWords,'array length',wordsArray.length);
+    for (let index = 1; index < wordsArray.length; index++) {
+      sequenceofWords += ' ';
+      sequenceofWords += wordsArray[index];
+    }
+    //console.log('before checking',sequenceofWords);
+    //console.log('word', word, 'sequenceOfWords', sequenceofWords);
+   // console.log(wordsArray);
+    if (word !== sequenceofWords) {
+      if (isEven(round)) {
+        pointsOfUser2++;
+      }
+      else {
+        pointsOfUser1++;
+      }
+    } else {
+      if (isEven(round)) {
+        pointsOfUser1++;
+      }
+      else {
+        pointsOfUser2++;
+      }
+    }
+    //console.log(wordsArray);
+    console.log('points of ', user1, pointsOfUser1, '\npoints of ', user2, pointsOfUser2, '\n');
+    //console.log('after checking',sequenceofWords);
   }
+  if (pointsOfUser1 > pointsOfUser2) {
+    winner = user1;
+  } else if (pointsOfUser1 < pointsOfUser2) {
+    winner = user2;
+  } else {
+    winner = user1 + ' and ' + user2;
+  }
+  console.log('Winner of the game is ', winner);
+
 }
 
 
