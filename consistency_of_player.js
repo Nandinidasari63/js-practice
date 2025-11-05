6dcbe7f2f3f724e4a6d3738d9e86832b4f064c7e
Nandinidasari63 <nandinidasari728@gmail.com>
2025-11-05 09:44:24 +0530

    Create consistency_of_player.js

---
+++ consistency_of_player.js
@@ -0,0 +1,82 @@
+function addAllElements(data){
+  let sum = 0;
+  for (let index = 0; index < data.length; index++) {
+    sum += data[index];
+  }
+  return sum;
+}
+
+
+function mean(scores) {
+ const sum = addAllElements(scores);
+ console.log(scores)
+  return sum / scores.length;
+}
+
+function sorted(data) {
+  const sortedData = data.slice();
+  for (let i = 0; i < sortedData.length; i++) {
+    for (let j = i + 1; j < sortedData.length; j++) {
+      if (sortedData[i] > sortedData[j]) {
+        let temp = sortedData[j];
+        sortedData[j] = sortedData[i];
+        sortedData[i] = temp;
+      }
+    }
+  }
+  return sortedData;
+}
+
+function distanceBetweenMean(scores,mean){
+  const distancesBetweenMeanForScores = [];
+    for (let index = 0; index < scores.length; index++) {
+      distancesBetweenMeanForScores.push(scores[index] - mean);     
+    }
+    return distancesBetweenMeanForScores;
+}
+
+function consistencyOfPlayer(scores,mean){
+  let sumOfSquares = 0;
+  for(let index = 0;index < scores.length; index++){
+      sumOfSquares += scores[index] ** 2;
+  }
+  console.log('sumOfSquares',sumOfSquares)
+  return Math.sqrt(sumOfSquares)/scores.length
+}
+
+function standardDeviation(scores,mean){
+  const absoluteValues = distanceBetweenMean(scores,mean);
+  console.log(absoluteValues)
+  const standardDeviationValue = consistencyOfPlayer(absoluteValues,mean);
+  return standardDeviationValue;
+}
+
+
+function median(scores){
+   const sortedScores = sorted(scores);
+   //console.log(sortedScores);
+   const len = sortedScores.length;
+   const  middleElement =(len - 1 )/ 2;
+   //console.log(sortedScores,len,middleElement);
+   const lowerElement = sortedScores[Math.floor(middleElement)];
+   const upperElement =  sortedScores[Math.ceil(middleElement)];
+   return (lowerElement + upperElement) / 2;
+}
+
+function statistics(scores) {
+ const meanValue = mean(scores);
+ console.log('mean',meanValue)
+  console.log('median',median(scores));
+  console.log(standardDeviation(scores,meanValue),'\n\n');
+}
+
+
+function testCases() {
+  statistics([5286,5589,7306,10449]);
+  statistics([91,102,111982,245026,120144,103217,978890,152563,117376]);
+  statistics([5005244,3428388,3851583,5733]);
+}
+function main() {
+  testCases();
+}
+main();
