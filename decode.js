52a971ed5400908723131598e13589deafeed1ef
Nandinidasari63 <nandinidasari728@gmail.com>
2025-11-05 17:53:14 +0530

    Update decode.js

--- decode.js
+++ decode.js
@@ -6,7 +6,6 @@ function decode(decodedData) {
       return encodeToNum(decodedData);
     case 'l':
       const output = [];
-      //console.log('\n i came into list');
       return encodeToList(decodedData, output)
     default:
       return encodeToStr(decodedData);
@@ -25,47 +24,42 @@ function encodeToStr(decodedData) {
   if (startIndex === -1 || Number.isNaN(lengthOfString)) {
     return true;
   }
-   console.log(startIndex,lengthOfString);
-     return [decodedData.slice(startIndex + 1, startIndex + 1 + lengthOfString), startIndex + 1 + lengthOfString];
-  }
+  return [decodedData.slice(startIndex + 1, startIndex + 1 + lengthOfString), startIndex + 1 + lengthOfString];
+}
 
 function encodeToList(decodedData, output) {
   let start = 1;
-  //console.log(decodedData.length - 1, output, start)
   while (start < decodedData.length - 1) {
-    // console.log('hii in loop');
-    //console.log(decodedData.slice(start, decodedData.length));
     let array = decode(decodedData.slice(start, decodedData.length));
     let element = array[0];
     if (array === true) {
       break;
     }
-    //console.log("element in array",element);
-
     output.push(element);
-    //console.log('output in each iteration',output);
     start += array[1];
-    //  console.log('starting', start);
   }
-  //console.log(output)
   return [output, start + 1];
 }
 
-console.log(decode("i123e")[0]);
-console.log(decode('i-42e')[0]);
-console.log(decode('i0e')[0]);
-console.log(decode('5:hello')[0]);
-console.log(decode("0:")[0]);
-console.log(decode("11:hello world")[0]);
-console.log(decode('26:abcdefghijklmnopqrstuvwxyz')[0]);
-console.log(decode("16:special!@#$chars")[0]);
-console.log(decode("l5:applei123e")[0]);
-console.log(decode("le")[0]);
-console.log(decode('l5:apple3:nan4:abcd7:nandinie')[0]);
-console.log(decode('li100ei200ei300ei400ee')[0]);
-console.log(decode('li100e5:applei700e5:nandu')[0]);
-console.log(decode("li0e0:l4:testee")[0]);
-console.log(decode("l0:i0ele")[0]);
-console.log(decode("l3:onel3:twol5:threeeee")[0]);
-console.log(decode('l3:onel3:twoei67ee')[0]);
-console.log(decode('l3:onel3:twoe5:threee')[0])
+function decodeFull(data) {
+  return decode(data)[0];
+}
+
+console.log(decodeFull("i123e"));
+console.log(decodeFull('i-42e'));
+console.log(decodeFull('i0e'));
+console.log(decodeFull('5:hello'));
+console.log(decodeFull("0:"));
+console.log(decodeFull("11:hello world"));
+console.log(decodeFull('26:abcdefghijklmnopqrstuvwxyz'));
+console.log(decodeFull("16:special!@#$chars"));
+console.log(decodeFull("l5:applei123e")[0]);
+console.log(decodeFull("le"));
+console.log(decodeFull("l5:apple3:nan4:abcd7:nandinie"));
+console.log(decodeFull('li100ei200ei300ei400ee'));
+console.log(decodeFull('li100e5:applei700e5:nandu'));
+console.log(decodeFull("li0e0:l4:testee"));
+console.log(decodeFull("l0:i0ele"));
+console.log(decodeFull("l3:onel3:twol5:threeeee"));
+console.log(decodeFull('l3:onel3:twoei67ee'));
+console.log(decodeFull('l3:onel3:twoe5:threee'))
